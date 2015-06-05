1ef84279fed652768bac34a1c3d2fc3cc45f5557
Philip Huysmans <philip.huysmans@gmail.com>
2015-06-05 17:20:17 +0200

    changing names

--- utils/imgManagement.py
+++ utils/imgManagement.py
@@ -9,7 +9,32 @@ def orderInDirectories():
 		dateString = filename[0:8]
 		timeString = filename[9:13]
 
-		print timeString
+		print "Timestring is: " + timeString
+
+		if(int(timeString[0:2]) < 12):
+			# Change the date to day before
+			if(int(dateString[6:8]) == 1):
+				# Convert to previous month
+				month = dateString[4:6]
+				#31-days months
+				if (month == 2) or (month == 4) or (month == 6) or (month == 8) or (month == 9) or (month == 11):
+					newDirName = dateString[0:4] + str(int(month)-1) + str(31)
+				#30-days months
+				if (month == 5) or (month == 7) or (month == 10) or (month == 12):
+					newDirName = dateString[0:4] + str(int(month)-1) + str(30)
+				#februari
+				if (month == 3):
+					newDirName = dateString[0:4] + str(int(month)-1) + str(28)
+				#januari
+				if (month == 1):
+					newDirName = str(int(dateString[0:4]-1)) + str(12) + str(31)
+			else:
+				# Normal day
+				newDirName = dateString[0:6] + int(dateString[6:8] - 1)
+		else:
+			newDirName = dateString
+
+		print "New Dir name is: " + newDirName
 		
 
 if __name__ == "__main__":
