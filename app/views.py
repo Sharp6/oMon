cda1ee73cfff0b29eabd080b41183fa759bf8044
Philip Huysmans <philip.huysmans@gmail.com>
2015-06-13 20:30:16 +0200

    added new template

--- app/views.py
+++ app/views.py
@@ -4,17 +4,10 @@ import zipfile
 from flask import render_template
 from app import app
 
-@app.route('/')
-@app.route('/index')
-def index():
+@app.route('/all')
+def allImg():
    imgs = sorted(os.listdir('./app/static/img'))
-   return render_template('index.html', title='O-Mon', images=imgs)
+   return render_template('all.html', title='O-Mon', images=imgs)
 
-@app.route('/zip')
-def zip():
-   zipf = zipfile.ZipFile('./tmp/img.zip', 'w')
-   files = os.listdir('./app/static/img')
-   for file in files:
-      zipf.write('./app/static/img/' + file)
-   zipf.close()
-   return "zip ok"
+@app.route('/')
+def 
