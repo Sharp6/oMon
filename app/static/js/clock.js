7bad49f9fd57278c1f0df1712a34864fcb382410
Philip Huysmans <philip.huysmans@gmail.com>
2015-06-14 15:33:00 +0200

    bug fix

--- app/static/js/clock.js
+++ app/static/js/clock.js
@@ -1,32 +1,32 @@
 $(document).ready(function() {
-ko.bindingHandlers.slider = {
-  init: function (element, valueAccessor, allBindingsAccessor) {
-    var options = allBindingsAccessor().sliderOptions || {};
-    $(element).slider(options);
-    ko.utils.registerEventHandler(element, "slidechange", function (event, ui) {
-        var observable = valueAccessor();
-        observable(ui.value);
-    });
-    ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
-        $(element).slider("destroy");
-    });
-    ko.utils.registerEventHandler(element, "slide", function (event, ui) {
-        var observable = valueAccessor();
-        observable(ui.value);
-    });
-  },
-  update: function (element, valueAccessor) {
-    var value = ko.utils.unwrapObservable(valueAccessor());
-    if (isNaN(value)) value = 0;
-    $(element).slider("value", value);
-  }
-};
+	ko.bindingHandlers.slider = {
+	  init: function (element, valueAccessor, allBindingsAccessor) {
+	    var options = allBindingsAccessor().sliderOptions || {};
+	    $(element).slider(options);
+	    ko.utils.registerEventHandler(element, "slidechange", function (event, ui) {
+	        var observable = valueAccessor();
+	        observable(ui.value);
+	    });
+	    ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
+	        $(element).slider("destroy");
+	    });
+	    ko.utils.registerEventHandler(element, "slide", function (event, ui) {
+	        var observable = valueAccessor();
+	        observable(ui.value);
+	    });
+	  },
+	  update: function (element, valueAccessor) {
+	    var value = ko.utils.unwrapObservable(valueAccessor());
+	    if (isNaN(value)) value = 0;
+	    $(element).slider("value", value);
+	  }
+	};
 
-var ViewModel = function() {
-    var self = this;
-    self.clockPosition = ko.observable(1);
-}
+	var ViewModel = function() {
+	    var self = this;
+	    self.clockPosition = ko.observable(1);
+	}
 
-ko.applyBindings(new ViewModel());
+	ko.applyBindings(new ViewModel());
 });
 
